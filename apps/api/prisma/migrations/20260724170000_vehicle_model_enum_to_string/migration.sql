1040a55f95a7c2bb84b9f0c109f519f1dd48914b
李小肥 <keqi@test.com>
2026-07-25 00:35:49 +0800

    refactor(vehicle): convert vehicle model enum columns to strings

---
+++ apps/api/prisma/migrations/20260724170000_vehicle_model_enum_to_string/migration.sql
@@ -0,0 +1,33 @@
+ALTER TABLE "vehicle_package"
+  ALTER COLUMN "vehicle_model" TYPE VARCHAR(64)
+  USING "vehicle_model"::text;
+
+ALTER TABLE "product_price_rule"
+  ALTER COLUMN "vehicle_model" TYPE VARCHAR(64)
+  USING "vehicle_model"::text;
+
+ALTER TABLE "vehicle"
+  ALTER COLUMN "vehicle_model" TYPE VARCHAR(64)
+  USING "vehicle_model"::text;
+
+ALTER TABLE "vehicle_model_definition"
+  ALTER COLUMN "legacy_vehicle_model" TYPE VARCHAR(64)
+  USING "legacy_vehicle_model"::text;
+
+ALTER TABLE "subscription_quote"
+  ALTER COLUMN "vehicle_model" TYPE VARCHAR(64)
+  USING "vehicle_model"::text;
+
+ALTER TABLE "subscription_quote"
+  ALTER COLUMN "legacy_vehicle_model_snapshot" TYPE VARCHAR(64)
+  USING "legacy_vehicle_model_snapshot"::text;
+
+ALTER TABLE "subscription_order"
+  ALTER COLUMN "vehicle_model" TYPE VARCHAR(64)
+  USING "vehicle_model"::text;
+
+ALTER TABLE "subscription_order"
+  ALTER COLUMN "legacy_vehicle_model_snapshot" TYPE VARCHAR(64)
+  USING "legacy_vehicle_model_snapshot"::text;
+
+DROP TYPE "vehicle_model";
